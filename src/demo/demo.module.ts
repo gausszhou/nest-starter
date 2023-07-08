5d6705c31be367f382306ad3b9ef9c4651ddf591
gausszhou <gausszhou@qq.com>
2023-07-08 23:07:24 +0800

    Remove demo module

--- src/demo/demo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DemoService } from './demo.service';
-import { DemoController } from './demo.controller';
-
-@Module({
-  providers: [DemoService],
-  controllers: [DemoController]
-})
-export class DemoModule {}
